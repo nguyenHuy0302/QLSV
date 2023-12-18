24aebecbc477be5e0610324bc1a1967af6c7f8b7
unknown <nguyendinhhuy0302@gmail.com>
2023-12-18 10:16:36 +0700

    up

---
+++ QL_SinhVien/QL_SinhVien/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QL_SinhVien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
